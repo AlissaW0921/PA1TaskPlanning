c5f536ea6f27a692da45cfec72a9d6022e286701
AlissaW0921 <92697920+AlissaW0921@users.noreply.github.com>
2024-09-08 18:12:02 -0400

    Added a logout button on MainActivity

--- app/src/main/java/com/example/pa1taskplanning/MainActivity.kt
+++ app/src/main/java/com/example/pa1taskplanning/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.pa1taskplanning
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonLogout: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var user: FirebaseUser
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = FirebaseAuth.getInstance()
+        buttonLogout = findViewById(R.id.logoutButton)
+        textView = findViewById(R.id.user_details)
+        user = auth.currentUser!!
+        if (user == null) {
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            textView.setText(user.email)
+        }
+
+        buttonLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }

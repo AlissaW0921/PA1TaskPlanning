fee01b833b74afd94a77995e54f64580732a344d
qye47 <yeqqww5109@gmail.com>
2024-09-09 21:50:02 -0400

    improved password requirements text layout

--- app/src/main/java/com/example/pa1taskplanning/Register.kt
+++ app/src/main/java/com/example/pa1taskplanning/Register.kt
@@ -89,4 +89,9 @@ class Register : AppCompatActivity() {
                 }
         }
     }
+    // Password validation function
+    private fun isValidPassword(password: String): Boolean {
+        val passwordRegex = Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$")
+        return passwordRegex.matches(password)
+    }
 }
